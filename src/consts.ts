ddb235c003e1d50fde87275f6c11cda5226587d8
Teodora Udrea <teodora.udrea1998@gmail.com>
2026-02-05 17:05:30 +0000

    email update

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = 'Teo Dev';
 export const SITE_DESCRIPTION = 'Personal portfolio showcasing selected work and writing.';
 export const SITE_AUTHOR = 'Teodora Udrea';
-export const SITE_EMAIL = 'teodora.udrea1998@gmail.com';
+export const SITE_EMAIL = 'teoudrea17@gmail.com';
 export const SITE_PHONE = '07892879040';
 export const SITE_LOCATION = 'St Andrews, UK';
 export const SITE_LINKEDIN = 'https://www.linkedin.com/in/teodora-udrea-620a9a11a/';
